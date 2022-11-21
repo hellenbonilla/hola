c4fc3372af6c749fb9f4b699b9f9c8fd88393a01
hellenbonilla <hellenbonilla.com@gmail.com>
2022-11-21 11:42:26 -0600

    agregar

---
+++ prin.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("en la arepa")
+st.write("Hola mundo")
+st.subheader("sub encabezado ")
+st.header("voladora")
+st.markdown("**Hola arepa**, **hola*,[ucr](https://www.ucr.ac.cr/)")
